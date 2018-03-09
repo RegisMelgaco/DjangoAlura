e99d3beade9c7a38bc32defc0e02c14a464f861f
Regis M <regis.melgaco@gmail.com>
2018-03-09 10:16:59 -0300

    aula 4

--- connectedin/perfis/models.py
+++ connectedin/perfis/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Perfil(models.Model):
-	nome = models.CharField(max_length = 50)
-	email = models.CharField(max_length = 50)
-	telefone = models.CharField(max_length = 20)
-	nome_empresa = models.CharField(max_length = 40)
+	nome = models.CharField(max_length = 50, null=False)
+	email = models.CharField(max_length = 50, null=False)
+	telefone = models.CharField(max_length = 20, null=False)
+	nome_empresa = models.CharField(max_length = 40, null=False)
