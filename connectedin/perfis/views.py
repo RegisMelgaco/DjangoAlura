5d8df25c778687c7e359f1d5ce9aa9500cc9cba6
Regis M <regis.melgaco@gmail.com>
2018-03-09 14:56:54 -0300

    aula 5

--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.urls import reverse
 
 from .models import Perfil
 
 def index(request):
-	return render(request, 'index.html')
+	perfil = get_list_or_404(Perfil)
+
+	return render(request, 'perfil/index.html', { 'perfis':perfil })
 
 def exibir(request, pk):
 	perfil = get_object_or_404(Perfil, pk=pk)
 
-	return render(request, 'perfil.html', { 'perfil':perfil })
+	return render(request, 'perfil/perfil.html', { 'perfil':perfil })
