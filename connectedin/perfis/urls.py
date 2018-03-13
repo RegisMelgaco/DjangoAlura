13b669b4ea747c6ebf6090053bc93cf5aaec6496
Regis M <regis.melgaco@gmail.com>
2018-03-13 08:10:12 -0300

    aula 6

--- connectedin/perfis/urls.py
+++ connectedin/perfis/urls.py
@@ -6,4 +6,5 @@ app_name = 'perfis'
 urlpatterns = [
     path('', views.index, name='index'),
     path('perfil/<int:pk>/', views.exibir, name='perfil'),
+    path('perfil/<int:pk>/convidar/', views.convidar, name='convidar')
 ]
--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404, redirect
 from django.urls import reverse
 
 from .models import Perfil
@@ -12,3 +12,12 @@ def exibir(request, pk):
 	perfil = get_object_or_404(Perfil, pk=pk)
 
 	return render(request, 'perfil/perfil.html', { 'perfil':perfil })
+
+def convidar(request, pk_convidado):
+	convidado = get_object_or_404(Perfil, pk=pk_convidado)
+	logado = usario_logado(request)
+	logado.convidar(convidado)
+	return redirect('index')
+
+def usario_logado(request):
+	return get_object_or_404(Perfil, pk=1)
